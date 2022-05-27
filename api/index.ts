65b35d64697a3679c76260e5a9ceabea4f93be43
Hamm <majhamm@qq.com>
2022-05-27 11:09:31 +0800

    fix(自动提交): 由Hamm通过自动脚本提交

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const body = {
-    request: request,
-    response: response
-  }
-  response.status(200).send(JSON.stringify(body));
+  response.status(200).send("Hello");
+  // const body = {
+  //   request: request,
+  //   response: response
+  // }
+  // response.status(200).send(JSON.stringify(body));
 };
