c0351141a1af4dfc3b560b025d517c1445f59e88
Hamm <majhamm@qq.com>
2022-05-27 11:19:39 +0800

    fix(自动提交): 由Hamm通过自动脚本提交

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import Http from '../http/Http'
 
-export default (request: VercelRequest, response: VercelResponse) => {
+export default async (request: VercelRequest, response: VercelResponse) => {
   const body = {
     request: request,
     response: response
   }
-  new Http("user/login").post().then((data) => {
-    response.status(200).send(JSON.stringify(data));
-  })
+  const result = await new Http("user/login").post()
+  response.status(200).send(JSON.stringify(result));
 };
