f7d1be1e5460953ed4f6054caf702119df002119
Hamm <majhamm@qq.com>
2022-05-27 11:43:13 +0800

    fix(自动提交): 由Hamm通过自动脚本提交

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 module.exports = (request, response) => {
+  console.log(request)
   axios.post('https://api3.bbbug.com/user/login', request.body)
     .then(res => {
       const data = res.data
